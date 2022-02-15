e4821e5b70541c34b407adf6a0326528be7d98ee
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-02-15 01:02:12 -0500

    feat: counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * max_array - calculates the max integer of the array
+ * @array: array
+ * @size: size of the @array
+ *
+ * Return: the max of the array
+ */
+size_t max_array(int *array, size_t size)
+{
+	size_t i, max;
+
+	max = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if ((int)max < array[i])
+			max = array[i];
+	}
+	return (max);
+}
+
+/**
+ * counting_sort - sorts an array of integers in ascending order
+ *		using the counting sort algorithm
+ * @array: array
+ * @size: size of the @array
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i, max;
+	int sum, *count, *output;
+
+	if (!array)
+		return;
+
+	max = max_array(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (!count)
+		return;
+
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+
+	sum = 0;
+	for (i = 0; i <= max; i++)
+	{
+		sum += count[i];
+		count[i] = sum;
+		if (i != max)
+			printf("%d, ", count[i]);
+	}
+	printf("%d\n", count[i - 1]);
+
+	output = malloc(sizeof(int) * (max + 1));
+	if (!count)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+}
